0c601c48d26ce27318e457810ec15af33de89e09
Junghwan John Goh <jhgoh@cern.ch>
2015-08-25 00:50:18 +0900

    first commit

---
+++ Muons/test/ex04.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from DataFormats.FWLite import Events, Handle,Lumis
+from ROOT import *
+gROOT.ProcessLine(".x tdrstyle.C")
+
+files = [
+    "file:/cmsdas/data/ShortEX_Muon/RelValZMM_13_MINIAODSIM/0AF36725-FB1A-E511-BCB2-0025905A497A.root",
+]
+
+events = Events(files)
+muonHandle = Handle('std::vector<pat::Muon>')
+for iEvent, event in enumerate(events):
+    print 'Analyzing', iEvent, 'th event'
+    event.getByLabel('slimmedMuons', muonHandle)
+    muons = muonHandle.product()
+    if muons.size() < 2: continue
+    muon1, muon2 = muons[0], muons[1]
+    if muon2.pt() <= 1: continue
+    print "Leading pt, eta, phi, charge = ", (muon1.pt(), muon1.eta(), muon1.phi(), muon1.charge())
+    print "Trailing pt, eta, phi, charge = ", (muon2.pt(), muon2.eta(), muon2.phi(), muon2.charge())
---
+++ Muons/test/ex05.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from DataFormats.FWLite import Events, Handle,Lumis
+from ROOT import *
+gROOT.ProcessLine(".x tdrstyle.C")
+
+hPt1 = TH1F("hPt1", "Muon1;Muon p_{T} (GeV);Events / 20GeV", 100, 0, 200)
+hPt2 = TH1F("hPt2", "Muon2;Muon p_{T} (GeV);Events / 20GeV", 100, 0, 200)
+hM = TH1F("hM", "Dimuon;Dimuon mass (GeV);Events / 20GeV", 100, 0, 200)
+hPt1.SetLineColor(kRed)
+hPt2.SetLineColor(kBlue)
+
+files = [
+    "file:/cmsdas/data/ShortEX_Muon/RelValZMM_13_MINIAODSIM/0AF36725-FB1A-E511-BCB2-0025905A497A.root",
+]
+
+events = Events(files)
+muonHandle = Handle('std::vector<pat::Muon>')
+for iEvent, event in enumerate(events):
+    print 'Analyzing', iEvent, 'th event'
+    event.getByLabel('slimmedMuons', muonHandle)
+    muons = muonHandle.product()
+    if muons.size() < 2: continue
+    muon1, muon2 = muons[0], muons[1]
+
+    hPt1.Fill(muon1.pt())
+    hPt2.Fill(muon2.pt())
+    dimuonP4 = muon1.p4()+muon2.p4()
+    hM.Fill(dimuonP4.mass())
+
+cPt = TCanvas("cPt", "cPt", 500, 500)
+hPt1.Draw()
+hPt2.Draw("same")
+cM = TCanvas("cM", "cM", 500, 500)
+hM.Draw()
